f87c696d06fa334d3064a3064c261bb4ac89c714
Robbie Eijkelkamp <43172249+Madlyaza@users.noreply.github.com>
2021-11-16 16:04:05 +0100

    Update

---
+++ Week2DierenApi2/src/main/java/com/resources/HelloWorld.java
@@ -0,0 +1,21 @@
+package com.resources;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+
+@RequestMapping("/helloWorld")
+public class HelloWorld {
+
+    @Value("")
+
+    @GetMapping
+    public String message()
+    {
+        System.out.println("hello world");
+        return "message";
+    }
+}
