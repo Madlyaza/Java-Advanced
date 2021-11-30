b1bf291e4f08d94cb2e4f83dd4af32da704c7629
Robbie Eijkelkamp <43172249+Madlyaza@users.noreply.github.com>
2021-11-30 11:01:28 +0100

    Update ExceptionHandlerController.java

--- Week2DierenApi2/src/main/java/com/exception/ExceptionHandlerController.java
+++ Week2DierenApi2/src/main/java/com/exception/ExceptionHandlerController.java
@@ -1,29 +1,14 @@
 package com.exception;
 
-import org.springframework.dao.DataAccessException;
-import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
-import org.springframework.web.servlet.ModelAndView;
-
-import javax.servlet.http.HttpServletRequest;
-import java.sql.SQLException;
 
 
 @RestControllerAdvice
 public class ExceptionHandlerController
 {
-//    @ResponseStatus(value = HttpStatus.CONFLICT,
-//            reason = "Data integrity violation")
-//    @ExceptionHandler({DataIntegrityViolationException.class, DataNotFoundException.class})
-//    public void conflict()
-//    {
-//
-//    }
-
     @ResponseStatus(HttpStatus.BAD_REQUEST)
     @ExceptionHandler(RuntimeException.class)
     public String runtimeHandler(RuntimeException ex)
@@ -44,12 +29,4 @@ public class ExceptionHandlerController
     {
         return ex.getMessage();
     }
-
-
-//
-//    @ExceptionHandler(Exception.class)
-//    public String handleError(HttpServletRequest req, RuntimeException ex) {
-//
-//        return "Error";
-//    }
 }
